04c4dcaf20761b60afa7daf9b52e79639620018c
LisaPape <lisa.pape@attempto.eu>
2023-03-06 11:01:18 +0100

    - add automatic Jest unit tests to crypto service

--- backend/src/crypto/crypto.service.spec.ts
+++ backend/src/crypto/crypto.service.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CryptoService } from './crypto.service';
+import { EncodeCryptoDto } from "./dto/encode-crypto.dto";
+import { DecodeCryptoDto } from "./dto/decode-crypto.dto";
+import axios from "axios";
+
 
 describe('CryptoService', () => {
   let service: CryptoService;
@@ -14,5 +18,30 @@ describe('CryptoService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+
+  it('should return an encoded message' , () => {
+    const encodeCryptoDto: EncodeCryptoDto = {
+      rawLosungswort: 'SCHWARZ',
+      rawInput: 'Hallo wie geht es Dir',
+    };
+    return axios.post('http://localhost:3000/crypto/encode', encodeCryptoDto)
+        .then((res) => {
+          expect(res.status).toBe(201);
+          expect(res.data).toBe('Hi aeel slg oeD hiwtr');
+        });
+  });
+
+  it('should return a decoded message', () => {
+    const decodeCryptoDto: DecodeCryptoDto = {
+      losungswort: 'SCHWARZ',
+      encodedInput: 'Hi aeel slg oeD hiwtr',
+    };
+    return axios.post('http://localhost:3000/crypto/decode', decodeCryptoDto)
+        .then((res) => {
+          expect(res.status).toBe(201);
+          expect(res.data).toBe('Hallo wie geht es Dir');
+        });
   });
 });
